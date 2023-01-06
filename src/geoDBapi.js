27524736ce4dc5ec90db08ae700e3b77bd5f8085
dominy xu <dominyxu@outlook.com>
2023-01-06 14:36:55 -0500

    search bar v1

---
+++ src/geoDBapi.js
@@ -0,0 +1,9 @@
+export const geoDBApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3b25498bf4msh9c004aba088714bp196bb0jsn81fd52d498d3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
