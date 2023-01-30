42d07fa8c87fbc060b90ee46b80d58bf54ca3568
dominyxu <dominyxu@outlook.com>
2023-01-29 21:33:45 -0500

    fixed icon

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -21,19 +21,19 @@ const CurrentWeather = ({ data }) => {
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Feels Like </span>
-                        <span className="parameter-label">{Math.round(data.main.feels_like)}°C</span>
+                        <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Wind </span>
-                        <span className="parameter-label">{data.wind.speed} m/s</span>
+                        <span className="parameter-value">{data.wind.speed} m/s</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Humidity </span>
-                        <span className="parameter-label">{data.main.humidity}%</span>
+                        <span className="parameter-value">{data.main.humidity}%</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Pressure </span>
-                        <span className="parameter-label">{data.main.pressure} hPa</span>
+                        <span className="parameter-value">{data.main.pressure} hPa</span>
                     </div>
                 </div>
             </div>
